3e74a8886968080a3de7ff0eb1df05aa68406f0d
Atanu Shil <atanushil358@gmail.com>
2025-05-01 23:19:17 +0530

    added the drag , drop and drop zone and swap

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ const config = {
   height: window.innerHeight,
   backgroundColor: '#87CEEB',
   scene: {
-    preload: preload,
-    create: create,
-    update: update
+    preload,
+    create,
+    update
   }
 };
 
@@ -18,11 +18,10 @@ let bg;
 let startButton;
 const tileCount = 11;
 let tileSprites = [];
-let selectedTile = null; // To keep track of the tile being dragged
 
 function preload() {
-  this.load.image('img', 'img.jpg');  
-  this.load.image('start', 'icons8-start-64.png');  
+  this.load.image('img', 'img.jpg');
+  this.load.image('start', 'icons8-start-64.png');
 }
 
 function create() {
@@ -30,8 +29,7 @@ function create() {
   bg.displayWidth = this.scale.width;
   bg.displayHeight = this.scale.height;
 
-  startButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'start').setOrigin(0.5, 0.5);
-  startButton.setScale(1);
+  startButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'start').setOrigin(0.5);
   startButton.setInteractive();
   startButton.on('pointerdown', () => sliceImage.call(this, 'img'));
 }
@@ -39,8 +37,8 @@ function create() {
 function sliceImage(imageKey) {
   bg.destroy();
   startButton.destroy();
-  const fullImage = this.textures.get(imageKey).getSourceImage();
 
+  const fullImage = this.textures.get(imageKey).getSourceImage();
   const tileWidth = config.width / tileCount;
   const tileHeight = config.height;
 
@@ -73,20 +71,71 @@ function sliceImage(imageKey) {
     sprite.currentIndex = i;
     tileSprites.push(sprite);
 
-
-
-    // Enable drag events
     this.input.setDraggable(sprite);
-
-    
-
-    
   }
 
-  
+  this.input.on('dragstart', (pointer, gameObject) => {
+    gameObject.setDepth(1);
+  });
+
+  this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+  });
+
+  this.input.on('dragend', (pointer, draggedTile) => {
+    let swapped = false;
+
+    for (let i = 0; i < tileSprites.length; i++) {
+      const targetTile = tileSprites[i];
+      if (targetTile === draggedTile) continue;
+
+      const boundsA = draggedTile.getBounds();
+      const boundsB = targetTile.getBounds();
+
+      if (Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB)) {
+        swapTiles(draggedTile, targetTile);
+        swapped = true;
+        break;
+      }
+    }
+
+    if (!swapped) {
+      resetTilePosition(draggedTile);
+    }
+  });
 }
 
+function resetTilePosition(tile) {
+  const tileWidth = config.width / tileCount;
+  tile.x = tile.currentIndex * tileWidth;
+  tile.y = 0;
+  tile.setDepth(0);
+}
+
+function swapTiles(tileA, tileB) {
+  const tileWidth = config.width / tileCount;
+
+  const indexA = tileA.currentIndex;
+  const indexB = tileB.currentIndex;
+
+  // Swap currentIndex
+  tileA.currentIndex = indexB;
+  tileB.currentIndex = indexA;
+
+  // Move them visually
+  tileA.x = indexB * tileWidth;
+  tileA.y = 0;
+  tileB.x = indexA * tileWidth;
+  tileB.y = 0;
+
+  tileA.setDepth(0);
+  tileB.setDepth(0);
+
+  // Swap in tileSprites array if needed
+  [tileSprites[indexA], tileSprites[indexB]] = [tileSprites[indexB], tileSprites[indexA]];
+}
 
 function update() {
-  // Game loop if needed
+  // Optional game loop
 }
