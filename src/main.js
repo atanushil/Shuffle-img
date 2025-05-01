e175f298a0b82d5254f2307c1c8a4934385dff44
Atanu Shil <atanushil358@gmail.com>
2025-05-01 23:37:39 +0530

    added the transtion in swapping two tile

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ const config = {
   height: window.innerHeight,
   backgroundColor: '#87CEEB',
   scene: {
-    preload,
-    create,
-    update
+    preload: preload,
+    create: create,
+    update: update
   }
 };
 
@@ -18,10 +18,11 @@ let bg;
 let startButton;
 const tileCount = 11;
 let tileSprites = [];
+let dropZones = [];
 
 function preload() {
-  this.load.image('img', 'img.jpg');
-  this.load.image('start', 'icons8-start-64.png');
+  this.load.image('img', 'img.jpg');  
+  this.load.image('start', 'icons8-start-64.png');  
 }
 
 function create() {
@@ -29,7 +30,8 @@ function create() {
   bg.displayWidth = this.scale.width;
   bg.displayHeight = this.scale.height;
 
-  startButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'start').setOrigin(0.5);
+  startButton = this.add.image(this.scale.width / 2, this.scale.height / 2, 'start').setOrigin(0.5, 0.5);
+  startButton.setScale(1);
   startButton.setInteractive();
   startButton.on('pointerdown', () => sliceImage.call(this, 'img'));
 }
@@ -43,6 +45,8 @@ function sliceImage(imageKey) {
   const tileHeight = config.height;
 
   const tileData = [];
+  tileSprites = [];
+  dropZones = [];
 
   for (let i = 0; i < tileCount; i++) {
     const canvas = document.createElement('canvas');
@@ -58,6 +62,12 @@ function sliceImage(imageKey) {
     const tileKey = `tile-${i}`;
     this.textures.addCanvas(tileKey, canvas);
     tileData.push({ key: tileKey, index: i });
+
+    // Draw outlined drop zone
+    const graphics = this.add.graphics();
+    graphics.lineStyle(2, 0xffa500, 1); // Orange outline
+    graphics.strokeRect(i * tileWidth, 0, tileWidth, tileHeight);
+    dropZones.push(graphics);
   }
 
   Phaser.Utils.Array.Shuffle(tileData);
@@ -72,16 +82,88 @@ function sliceImage(imageKey) {
     tileSprites.push(sprite);
 
     this.input.setDraggable(sprite);
-
-    
-
-    
   }
 
-  
+  // Drag events
+  this.input.on('dragstart', (pointer, gameObject) => {
+    gameObject.setDepth(1);
+  });
+
+  this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+    gameObject.x = dragX;
+    gameObject.y = dragY;
+  });
+
+  this.input.on('dragend', (pointer, draggedTile) => {
+    let swapped = false;
+
+    for (let i = 0; i < tileSprites.length; i++) {
+      const targetTile = tileSprites[i];
+      if (targetTile === draggedTile) continue;
+
+      const boundsA = draggedTile.getBounds();
+      const boundsB = targetTile.getBounds();
+
+      if (Phaser.Geom.Intersects.RectangleToRectangle(boundsA, boundsB)) {
+        swapTiles(draggedTile, targetTile);
+        swapped = true;
+        break;
+      }
+    }
+
+    if (!swapped) {
+      resetTilePosition(draggedTile);
+    }
+  });
 }
 
+// Smooth reset tile position when dropped outside valid area
+function resetTilePosition(tile) {
+  const tileWidth = config.width / tileCount;
+  tile.scene.tweens.add({
+    targets: tile,
+    x: tile.currentIndex * tileWidth,
+    y: 0,
+    duration: 300,
+    ease: 'Power2',
+    onComplete: () => tile.setDepth(0)
+  });
+}
+
+// Swap tiles with smooth transition using tweens
+function swapTiles(tileA, tileB) {
+  const tileWidth = config.width / tileCount;
+
+  const indexA = tileA.currentIndex;
+  const indexB = tileB.currentIndex;
+
+  // Swap current index
+  tileA.currentIndex = indexB;
+  tileB.currentIndex = indexA;
+
+  // Tween animations to new positions
+  tileA.scene.tweens.add({
+    targets: tileA,
+    x: indexB * tileWidth,
+    y: 0,
+    duration: 300,
+    ease: 'Power2',
+    onComplete: () => tileA.setDepth(0)
+  });
+
+  tileB.scene.tweens.add({
+    targets: tileB,
+    x: indexA * tileWidth,
+    y: 0,
+    duration: 300,
+    ease: 'Power2',
+    onComplete: () => tileB.setDepth(0)
+  });
+
+  // Swap in array
+  [tileSprites[indexA], tileSprites[indexB]] = [tileSprites[indexB], tileSprites[indexA]];
+}
 
 function update() {
-  // Optional game loop
+  // Optional game logic
 }
